3b64f21b61c7bb704dcb83e7a84d7431ef2fe4cf
Jeroen van den Heuvel <jeroen.van.den.heuvel@werkspot.nl>
2018-07-05 14:37:52 +0200

    Use kubernetes to spin up the containers and link them

--- webserver/index.php
+++ webserver/index.php
@@ -5,6 +5,7 @@
 <body>
 <h1>Hello world</h1>
 <p>The time is currently <time datetime="<?= date(DATE_ATOM); ?>"><?= date('Y-m-d H:i:s') ?></time>.</p>
+<p><?= mt_rand(0, PHP_INT_MAX); ?></p>
 <time></time>
 </body>
 </html>
