676a0206f512852b4a9cfc128bfb07ca9049fb1e
Jeroen van den Heuvel <jeroen.van.den.heuvel@werkspot.nl>
2018-07-05 11:59:47 +0200

    Create proxy + webserver to test a multi container setup.

---
+++ webserver/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Hello world</title>
+</head>
+<body>
+<h1>Hello world</h1>
+<p>The time is currently <time datetime="<?= date(DATE_ATOM); ?>"><?= date('Y-m-d H:i:s') ?></time>.</p>
+<time></time>
+</body>
+</html>
